c4397604bb792ecdbe3d2a21f1c102565b4b5dc8
Jacob Egge <jacob.egge@web.de>
2019-07-07 17:37:39 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -18,20 +18,22 @@
   <input type="submit" value="Turn off humidifier">
 </form>
 
-<input type="submit" class="button" name="insert" value="insert" />
-
+<button type="button">Click Me</button>
+<p></p>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
-	$(document).ready(function(){
-    $('.button').click(function(){
-        //var clickBtnValue = $(this).val();
-        var ajaxurl = 'humidifier_on.php',
-        data =  {};
-        $.post(ajaxurl, data, function (response) {
-            // Response div goes here.
-            alert("action performed successfully");
-        });
-    });
+    $(document).ready(function(){
+        $("button").click(function(){
 
+            $.ajax({
+                type: 'POST',
+                url: 'humidifier_on.php',
+                success: function(data) {
+                    alert(data);
+                    $("p").text(data);
+
+                }
+            });
+   });
 });
 </script>
 	
