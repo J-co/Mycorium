e7934bf2c7e05e6717b41a232dff31cd146f81cb
Jacob Egge <jacob.egge@web.de>
2019-07-07 19:19:06 +0200

    Update humidifier_on.php

--- html/humidifier_on.php
+++ html/humidifier_on.php
@@ -1,6 +1,6 @@
 <?php 
 function humidifier_on(){
-$command = '/home/pi/Projects/Mycorium/humidifier_on.py 2>&1';
+$command = '/home/pi/Projects/Mycorium/set_GPIO.py 13 HIGH 2>&1';
 exec($command, $out, $status);
 echo join(', ', $out);
 }
