759e52e1a8a42454f14b748aa1726c1b74261a55
Jacob Egge <jacob.egge@web.de>
2019-07-13 19:26:30 +0200

    sensor stuff

--- python_server/server.py
+++ python_server/server.py
@@ -1,39 +1,51 @@
+
+import Adafruit_DHT
+import RPi.GPIO as GPIO
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
- 
+
 from tornado.options import define, options
 define("port", default=8080, help="run on the given port", type=int)
- 
 
-import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-humidifier_pin = 13
-GPIO.setup(humidifier_pin,GPIO.OUT)
+# Humidifier Setup
+humidifierPin = 13
+GPIO.setup(humidifierPin, GPIO.OUT)
+GPIO.output(humidifierPin, GPIO.HIGH)  # disable humidifier on start up
+
+# Adafruit Sensor Setup
+sensor = Adafruit_DHT.DHT11
+adafruitPin = 17
+
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
- 
+
+
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print 'new connection'
         self.write_message("connected")
- 
+
     def on_message(self, message):
         print 'message received %s' % message
         self.write_message('message received %s' % message)
-        if message=="humidifier_on":            
-            GPIO.output(humidifier_pin,GPIO.LOW)
-        if message=="humidifier_off":            
-            GPIO.output(humidifier_pin,GPIO.HIGH)
- 
+        if message == "HUMIDIFIER_ON":
+            humidifierOn()
+        if message == "HUMIDIFIER_OFF":
+            humidifierOff()
+        if message == "ADAFRUIT_READ":
+            readAdafruitSensor()
+
     def on_close(self):
         print 'connection closed'
- 
+
+
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     app = tornado.web.Application(
@@ -46,3 +58,22 @@ if __name__ == "__main__":
     httpServer.listen(options.port)
     print "Listening on port:", options.port
     tornado.ioloop.IOLoop.instance().start()
+
+
+# RPi functions
+
+def humidifierOn():
+    GPIO.output(humidifierPin, GPIO.LOW)
+
+
+def humidifierOff():
+    GPIO.output(humidifierPin, GPIO.HIGH)
+
+
+def readAdafruitSensor():
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, adafruitPin)
+
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
