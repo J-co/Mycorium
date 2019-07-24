7273964285d9ae3cd086e778a8ed39df8bf6aa00
Jacob Egge <jacob.egge@web.de>
2019-07-24 18:30:43 +0200

    Update server.py

--- python_server/server.py
+++ python_server/server.py
@@ -10,7 +10,7 @@ import tornado.websocket
 import json
 
 from tornado.options import define, options
-define("port", default=8080, help="run on the given port", type=int)
+define("port", default=80, help="run on the given port", type=int)
 
 MyControl = Controller()
 
